349ec81c24d06edf63e519773f458bdc7d388b86
RLcero29 <rlblucero@gmail.com>
2025-02-08 19:43:54 -0800

    Add files via upload

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,110 @@
+// Create the 'basemap' tile layer that will be the background of our map.
+let baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+
+// OPTIONAL: Step 2
+// Create the 'street' tile layer as a second background of the map
+
+
+// Create the map object with center and zoom options.
+let map = L.map("map", {
+  center: [
+    39.8282, -98.5795
+  ],
+  zoom: 4,
+
+// Then add the 'basemap' tile layer to the map.
+  layers: [baseMap]});
+
+// OPTIONAL: Step 2
+// Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
+// Add a control to the map that will allow the user to change which layers are visible.
+
+
+// Make a request that retrieves the earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. Pass the magnitude and depth of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      fillOpacity: 1,
+      color: "black",
+      fillColor : getColor(feature.geometry.coordinates[2]),
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    }
+  }
+
+  // This function determines the color of the marker based on the depth of the earthquake.
+  function getColor(depth) {
+    if (depth < 10) return "#e5f5e0"
+    else if (depth >= 10 & depth < 30) return "#a1d99b"
+    else if (depth >= 30 & depth < 50) return "#31a354"
+    else if (depth >= 50 & depth < 70) return "#ffeda0"
+    else if (depth >= 70 & depth < 90) return "#feb24c"
+    else if (depth >= 90) return "#f03b20"
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  function getRadius(magnitude) {
+    return magnitude * 4
+  }
+
+  // Add a GeoJSON layer to the map once the file is loaded.
+  L.geoJson(data, {
+    // Turn each feature into a circleMarker on the map.
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng)
+    },
+    // Set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("Magnitude of earthquake: " + feature.properties.mag + "<br>Location of earthquake: " + feature.properties.place)
+    }
+  // OPTIONAL: Step 2
+  // Add the data to the earthquake layer instead of directly to the map.
+  }).addTo(map);
+
+  // Create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  // Then add all the details for the legend
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    // Initialize depth intervals and colors for the legend
+        grades = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+    
+
+    // Loop through our depth intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+          '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+    
+    return div;
+  };
+
+  // Finally, add the legend to the map.
+  legend.addTo(map);
+
+  // OPTIONAL: Step 2
+  // Make a request to get our Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
+    // Save the geoJSON data, along with style information, to the tectonic_plates layer.
+
+
+    // Then add the tectonic_plates layer to the map.
+
+  });
+});
